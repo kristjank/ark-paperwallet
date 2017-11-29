78884aee5a2658cf3810266b76ad09015ccde07a
chris <kristjan.kosic@adora-med.com>
2017-11-29 10:47:01 +0100

    money generation and tx sending

--- src/wallet.js
+++ src/wallet.js
@@ -3,6 +3,12 @@ import crypto from 'crypto'
 
 import bip39 from 'bip39'
 import arkjs from 'arkjs'
+import request from 'request'
+
+var callback = function(error, response, body) {
+    console.log(error || body);
+  };
+
 
 angular.module('wallet', [])
   .factory('wallet', () => {
@@ -19,6 +25,23 @@ angular.module('wallet', [])
         let publicKey = ecpair.getPublicKeyBuffer().toString('hex')
         let address = ecpair.getAddress().toString('hex')
         let wif = ecpair.toWIF()
+
+        var amount      = 50 * Math.pow(10, 8); 
+        arkjs.crypto.setNetworkVersion(30)
+        var transaction = arkjs.transaction.createTransaction(address, amount, "DARK paper wallet - now start coding:)", "post throw venue dove boss mule amount pencil coach crisp purpose slice", "");
+        request({
+          url: 'http://104.238.165.129:4002/peer/transactions',
+          json: { transactions: [transaction] },
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'os': 'linux3.2.0-4-amd64',
+            'version': '0.3.0',
+            'port': 4002,
+            'nethash': "578e820911f24e039733b45e4882b73e301f813a0d2c31330dafda84534ffa23"
+          }
+        }, callback);
+  
 
         return {
           passphrase,
